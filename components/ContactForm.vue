3485c53575b926b4dd0c93e36b4d1f92a11662b2
iddar <iddar@dbug.mx>
2018-03-15 02:32:43 -0600

    include new form

---
+++ components/ContactForm.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="form-container">
+    <form v-on:submit.prevent="onSubmit" name="contact" method="POST" action="thank-you" netlify>
+      <label>Email:</label>
+      <input type="text" name="name">
+      <label>Message:</label>
+      <textarea name="message"></textarea>
+      <div netlify-recaptcha></div>
+      <button type=”submit”>Send</button>
+    </form>
+  </div>
+</template>
+
+<script>
+  export default {
+    methods: {
+      onSubmit: function ({target}) {
+        var formData = new FormData(target);
+        fetch(target.getAttribute('action'), {
+          method: 'POST',
+          body: formData
+        })
+      }
+    }
+  }
+</script>
+
+<style>
+  .form-container {
+
+  }
+
+  .form-container form {
+    display: flex;
+    flex-direction: column;
+  }
+
+  .form-container input {
+    display: flex;
+    flex-direction: column;
+  }
+</style>
