b23958a2e26c5ea1570ef3386564de685f313e86
iddar <iddar@dbug.mx>
2018-03-15 01:21:32 -0600

    init

---
+++ pages/index.vue
@@ -0,0 +1,65 @@
+<template>
+  <section class="container">
+    <div class="center">
+      <h1 class="title">
+        crossover<span>X</span>developer
+      </h1>
+      <a href="https://medium.com/@iddar/descubriendo-un-nuevo-camino-135202f816ca">HISTORY</a>
+    </div>
+  </section>
+</template>
+
+<script>
+// <!-- <app-logo /> -->
+// import AppLogo from '~/components/AppLogo.vue'
+
+export default {
+  components: {
+    // AppLogo
+  },
+  head: {
+    link: [
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Mina:400,700' }
+    ]
+  }
+}
+</script>
+
+<style>
+body {
+  background: url('~/assets/background.jpeg') center top;
+  color: #ffffff;
+  font-family: 'Mina', sans-serif;
+  height: 100vh;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.center {
+  text-align: center;
+}
+
+.title {
+  font-size: 2.5rem;
+  color: #e0d5d8ad;
+  cursor: default;
+  transition: color ease .3s;
+}
+
+.title:hover {
+  color: #fff;
+}
+
+.title span {
+  font-size: 3em;
+  font-weight: 700;
+  vertical-align: middle;
+}
+
+a {
+  color: #fff;
+}
+
+</style>
